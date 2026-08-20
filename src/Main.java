97d4322a357d01497f83717d69d7ee1ad282a4bf
Silas Barbosa <silasbarbosademorais@gmail.com>
2026-08-20 20:06:04 -0300

    Att

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
-import br.com.pessoal.RotaFacil.modelos.Veiculo;
 
 void main() {
 
-    Veiculo carro = new Veiculo();
+  }
 
-
-    carro.registraViagem(20);
-    carro.registraViagem(5);
-    carro.mediaKmPorVIagem();
-
-}
